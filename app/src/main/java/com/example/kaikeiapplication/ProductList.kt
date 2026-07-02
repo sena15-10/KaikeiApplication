3539f06e29fb9446a8d14c2766f4b7ddbb2a0571
jr2504 <8119422194ema@gmail.com>
2026-07-02 14:42:59 +0900

    再度、商品登録可能

--- app/src/main/java/com/example/kaikeiapplication/ProductList.kt
+++ app/src/main/java/com/example/kaikeiapplication/ProductList.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +36,15 @@ class ProductList : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_product_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val addButton = view.findViewById<FloatingActionButton>(R.id.fadAddProduct2)
+        addButton?.setOnClickListener {
+            (activity as? MainActivity)?.replaceFragment(SaveItemFragment())
+        }
     }
 
     companion object {
